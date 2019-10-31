f7a05f3b3e284326304e3bcccfac5b8c2c350eb9
Alex Grace <alexgrace@Alexs-MacBook-Pro.local>
2019-10-30 23:18:35 -0400

    home and login changes with navigation

--- mixedNewsExpo/screens/Home.js
+++ mixedNewsExpo/screens/Home.js
@@ -15,7 +15,74 @@ return (
 <ScrollView >
 
     <View style={styles.container}>
-    <Text style={{fontSize:25}}>WELL BALANCED</Text>
+
+    <Text style={{fontSize:25, fontWeight:800, marginBottom:10}}>WELL BALANCED</Text>
+    <Text style={{fontSize:15, marginBottom:10}}> Updated: 10/30/2019 </Text>
+
+
+    <View style={styles.topicBox}>
+        <View style={styles.topicBar}>
+            <Text style={styles.topic}> Trending</Text>
+        </View>
+        <Text style={styles.articleBlue}> 
+            https://arandomsite.com/article/that/is/very/conservative/
+        </Text>
+        <Text style={styles.articleRed}> 
+            https://arandomsite.com/article/that/is/very/liberal
+        </Text>
+    </View>
+
+    <View style={styles.topicBox}>
+        <View style={styles.topicBar}>
+            <Text style={styles.topic}> Climate Change</Text>
+        </View>
+        <Text style={styles.articleBlue}> 
+            https://arandomsite.com/article/that/is/very/conservative/
+        </Text>
+        <Text style={styles.articleRed}> 
+            https://arandomsite.com/article/that/is/very/liberal
+        </Text>
+    </View>
+
+    <View style={styles.topicBox}>
+        <View style={styles.topicBar}>
+            <Text style={styles.topic}> Gun Control</Text>
+        </View>
+        <Text style={styles.articleBlue}> 
+            https://arandomsite.com/article/that/is/very/conservative/
+        </Text>
+        <Text style={styles.articleRed}> 
+            https://arandomsite.com/article/that/is/very/liberal
+
+        </Text>
+    </View>
+
+    <View style={styles.topicBox}>
+        <View style={styles.topicBar}>
+            <Text style={styles.topic}> Politics</Text>
+        </View>
+        <Text style={styles.articleBlue}> 
+            https://arandomsite.com/article/that/is/very/conservative/
+        </Text>
+        <Text style={styles.articleRed}> 
+            https://arandomsite.com/article/that/is/very/liberal
+
+        </Text>
+    </View>
+
+    <View style={styles.topicBox}>
+        <View style={styles.topicBar}>
+            <Text style={styles.topic}> Economy </Text>
+        </View>
+        <Text style={styles.articleBlue}> 
+            https://arandomsite.com/article/that/is/very/conservative/
+        </Text>
+        <Text style={styles.articleRed}> 
+            https://arandomsite.com/article/that/is/very/liberal
+
+        </Text>
+    </View>
+
     </View>
 
 </ScrollView>
@@ -33,4 +100,39 @@ const styles = StyleSheet.create({
       justifyContent:'space-between',
       backgroundColor: '#FFFFFF',
     },
+    topic: {
+        fontSize: 20,
+        letterSpacing: 4,
+        fontFamily:"Futura-Medium",
+        textTransform:"uppercase",
+    },
+    topicBox:{
+        display: 'flex',
+        alignItems:'center',
+        marginTop: 20,
+        marginBottom:20,
+
+    },
+    topicBar:{
+        width:300,
+         alignItems:'center',
+         backgroundColor:"#DDDDDD",
+         borderRadius: 35,
+    },
+    articleBlue:{
+        marginTop: 8,
+        marginBottom: 8,
+        marginRight: 8,
+        marginLeft:8,
+        color: '#0074D9',
+        textAlign:'center',
+    },
+    articleRed:{
+        marginTop: 8,
+        marginBottom: 8,
+        marginRight: 8,
+        marginLeft:8,
+        color: '#b94a48',
+        textAlign:'center',
+    }
 });
--- mixedNewsExpo/screens/Login.js
+++ mixedNewsExpo/screens/Login.js
@@ -25,8 +25,13 @@ export default class Login extends Component {
       <View style={styles.container}>
         <Image source={logo} style={styles.logo}/>
         <Text style={styles.welcome}>Welcome to Well Balanced!</Text>
-        <Text style={styles.about}>This is a description of the app and its purpose of existence.</Text>
-        <Text style={styles.instructions}>To get started, login to the app with your google account! </Text>
+        <Text style={styles.about}>
+            A news app that provides conservative and 
+            liberal articles on the same topic. Explore 
+            constrasting opinions in politics today.
+        
+        </Text>
+        <Text style={styles.instructions}>To get started, login to the app with your Google account! </Text>
         <TouchableOpacity onPress={this.onPressLogin}>
             <Image source={signin} style={styles.signin} />
         </TouchableOpacity>
